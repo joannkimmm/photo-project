225f0272fa4f9b4370ab38d065329671907018d9
Jeremy Kao <j7kao@ucsd.edu>
2014-02-15 14:42:00 -0800

    swag

--- public_html/js/feed.js
+++ public_html/js/feed.js
@@ -1,11 +1,20 @@
 var urlHash = window.location.hash;
 if (urlHash){
   console.log(urlHash);
-}
-var feedEl = $('#feed');
-$.ajax({
-  url: "https://api.instagram.com/v1/users/self/feed",
-})
+  var feedEl = $('#feed');
+  $.ajax({
+    url: "https://api.instagram.com/v1/users/self/feed",
+  })
   .done(function(response){
     console.log(response);
   });
+}
+else{
+  //login
+  var igLoginButton = document.querySelectorAll('#ig_login');
+  console.log(igLoginButton);
+  igLoginButton[0].addEventListener('click', function(){
+  console.log('clicked login button');
+  var redirect_uri = "http://photo-project.herokuapp.com/loggedIn"
+  window.location.href="https://instagram.com/oauth/authorize/?client_id=f2654ca86bdb473f907f7c2412ab7696&redirect_uri="+ redirect_uri +"&response_type=token";}, false);
+}
