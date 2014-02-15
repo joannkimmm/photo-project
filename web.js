cde3a6b23612c31851cd0e4d195915c266552ed8
Jeremy Kao <j7kao@ucsd.edu>
2014-02-15 14:12:05 -0800

    adding

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/public_html'));
 
-app.get("/loggedIn/", express.static(__dirname + '/public_html/feed.html'));
+app.get("/loggedIn", 'feed.html');
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
