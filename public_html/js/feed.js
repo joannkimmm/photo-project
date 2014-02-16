0d25bf5d99baacdae5a55aad6533388067ad651e
Joann Kim <jnk006@ucsd.edu>
2014-02-15 18:40:04 -0800

    user info test

--- public_html/js/feed.js
+++ public_html/js/feed.js
@@ -4,8 +4,8 @@ if (urlHash){
   var access_token = urlHash.substring(urlHash.indexOf(delimiter)+delimiter.length);
   var feedEl = $('#feed');
   $.ajax({
-    url: "https://api.instagram.com/v1/users/self",
-    //url: "https://api.instagram.com/v1/users/self/media/recent",
+    url: "https://api.instagram.com/v1/users/self/username",
+    url: "https://api.instagram.com/v1/users/self/media/recent",
     data: {'access_token': access_token},
     dataType: 'jsonp'
   }).done(function(response){
